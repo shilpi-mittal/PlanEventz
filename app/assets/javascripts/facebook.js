a57ab26ed4f56fb7ca3e346d6296bce042a3cd48
shilpi <shilpi.ec1991@gmail.com>
2015-06-28 02:15:49 +0530

    adding logout fb button

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -24,7 +24,6 @@ function statusChangeCallback(response) {
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
-        testAPI();
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
@@ -84,14 +83,8 @@ window.fbAsyncInit = function() {
 
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
+function getUserName() {
     FB.api('/me', function(response) {
-        console.log('Successful login for: ' + response.status);
-        console.log('Successful login for: ' + response.name);
-        console.log('Successful login for: ' + response.email);
-        console.log('Successful login for: ' + response.first_name);
-        document.getElementById('status').innerHTML =
-            'Thanks for logging in, ' + response.name + '!';
+        return(response.name);
     });
 }
