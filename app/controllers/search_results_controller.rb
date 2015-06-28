153827ebb24264c0c8d003506402e1193ad3ae74
shilpi <shilpi.ec1991@gmail.com>
2015-06-28 22:41:44 +0530

    fixing search functionality - removing sub category results

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -18,17 +18,17 @@ class SearchResultsController < ApplicationController
       end
     end
 
-    @vendor_sub_category_query = VendorSubCategory.search do
-      fulltext params[:search]
-    end
-    if @vendor_sub_category_query.results.length != 0
-      @vendor_sub_category_query.results.each do |vendor_sub_category|
-        vendor_sub_category_id = VendorSubCategory.find_by_sub_category(vendor_sub_category["sub_category"])["id"]
-        vendor_by_sub_category_query = Vendor.where("vendor_sub_category_id=?",vendor_sub_category_id)
-        @vendor = @vendor + vendor_by_sub_category_query
-        @vendor.uniq!
-      end
-    end
+    # @vendor_sub_category_query = VendorSubCategory.search do
+    #   fulltext params[:search]
+    # end
+    # if @vendor_sub_category_query.results.length != 0
+    #   @vendor_sub_category_query.results.each do |vendor_sub_category|
+    #     vendor_sub_category_id = VendorSubCategory.find_by_sub_category(vendor_sub_category["sub_category"])["id"]
+    #     vendor_by_sub_category_query = Vendor.where("vendor_sub_category_id=?",vendor_sub_category_id)
+    #     @vendor = @vendor + vendor_by_sub_category_query
+    #     @vendor.uniq!
+    #   end
+    # end
 
   end
 
