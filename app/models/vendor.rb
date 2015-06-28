7836aeaf920d1620f42b1aa1130f597d42ef2266
shilpi <shilpi.ec1991@gmail.com>
2015-06-28 19:35:40 +0530

    putting in validations for vendor fields and adding custom messages

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -9,7 +9,11 @@ class Vendor < ActiveRecord::Base
 
   validates :name, presence: true
   validates :location, presence: true
-  validates :phone, presence: true
+  validates :phone, presence: true,
+            numericality: { only_integer: true },
+            length: {minimum: 10, maximum: 11}
   validates :category_id, presence: true
+  validates :events_handled, numericality: { only_integer: true },
+            allow_blank: true
 
 end
