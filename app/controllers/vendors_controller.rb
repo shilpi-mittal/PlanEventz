45c61683b4cb02358ae090f324d0c21ac3e2397a
shilpi <shilpi.ec1991@gmail.com>
2015-06-29 00:03:00 +0530

    adding option to add multiple gallery pics while adding new vendor

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -14,27 +14,24 @@ class VendorsController < ApplicationController
     @vendor = Vendor.new
   end
 
-  def picToBlob(image_file)
-    begin
-      fin = File.open image_file, "rb"
-      img = fin.read
-    rescue SystemCallError => e
-      puts e
-    ensure
-      fin.close if fin
-    end
-
-    return SQLite3::Blob.new img
-  end
-
   def create
     @vendor = Vendor.new(vendor_params)
-    uploaded_io = params[:vendor][:cover_pic]
-    if uploaded_io!=nil
-      @vendor.cover_pic = SQLite3::Blob.new uploaded_io.read
-    end
-
     if @vendor.save
+      uploaded_io = params[:vendor][:cover_pic]
+      if uploaded_io!=nil
+        @vendor.cover_pic = SQLite3::Blob.new uploaded_io.read
+      end
+
+      uploaded_io = params[:vendor][:gallery_pic]
+      if uploaded_io!=nil
+        uploaded_io.each do |pic|
+          @vendor_photo = VendorPhoto.new
+          @vendor_photo.vendor_id = @vendor.id
+          @vendor_photo.photo = SQLite3::Blob.new pic.read
+          @vendor_photo.save
+        end
+      end
+
       redirect_to vendor_path(@vendor.id)
     else
       render 'new'
