c04f5e438f52aecaea9890ae9db66987b5da282b
shilpi <shilpi.ec1991@gmail.com>
2015-07-17 15:30:25 +0530

    fixing bug - search results showing unverified vendors

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -3,18 +3,32 @@ class SearchResultsController < ApplicationController
     @vendor_query = Vendor.search do
       fulltext params[:search]
     end
-    @vendor = @vendor_query.results
-    @vendor.uniq!
+
+    if @vendor_query.results.length != 0
+      @vendor_query.results.each do |vendor|
+        vendor_result = Vendor.where("id=? and is_verified=?", vendor["id"], true)
+        if @vendor!=nil
+          @vendor = @vendor + vendor_result
+          @vendor.uniq!
+        else
+          @vendor = vendor_result
+        end
+      end
+   end
 
     @vendor_category_query = VendorCategory.search do
       fulltext params[:search]
     end
     if @vendor_category_query.results.length != 0
-      @vendor_category_query.results.each do |vendor_category|
-        vendor_category_id = VendorCategory.find_by_category(vendor_category["category"])["id"]
-        vendor_by_category_query = Vendor.where("category_id=?",vendor_category_id)
-        @vendor = @vendor + vendor_by_category_query
-        @vendor.uniq!
+        @vendor_category_query.results.each do |vendor_category|
+          vendor_category_id = VendorCategory.find_by_category(vendor_category["category"])["id"]
+          vendor_by_category_query = Vendor.where("category_id=? and is_verified=?",vendor_category_id, true)
+          if @vendor!=nil
+            @vendor = @vendor + vendor_by_category_query
+            @vendor.uniq!
+          else
+            @vendor = vendor_by_category_query
+          end
       end
     end
 
